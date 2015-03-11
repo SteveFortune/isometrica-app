774d7331284d31732bcb1225228a93dc2e953a22
Mark Leusink <m.leusink@gmail.com>
2015-03-11 21:00:10 +0100

    delete canvas items when a plan is deleted

--- common/models/plan.js
+++ common/models/plan.js
@@ -1,35 +1,31 @@
 module.exports = function(Plan) {
-/*
-	
-	Plan.observe('before delete', function(ctx, next) {
-	console.log(ctx.where)
-		console.log('c' + ctx.where['id']);
 
-		app.models.CanvasItem.find( { where : {planId : ctx.where.id } }, function(err, items) {
-			console.log('b');
-			console.log('found ' + items.length + ' related');
+	Plan.observe('before delete', function (ctx, next) {
 
-		} )
+		//delete all canvas item in this plan
+		var CanvasItem = ctx.Model.app.models.CanvasItem;
 
-		//console.log(CanvasItem);
+		CanvasItem.find({
+			where: {
+				planId: ctx.where.id
+			}
+		}, function (err, items) {
+			items.forEach(function (item) {
+				CanvasItem.destroyById(item.id, function () {
 
-	  	//console.log('Going to delete %s matching %j',
-	   // ctx.Model.pluralModelName,
-	   // ctx.where);
+				});
+			});
+		});
 
-	  next();
-	});*/
+		next();
+	});
 
 	Plan.observe('before save', function updateTimestamp(ctx, next) {
 
-		// console.log('Going to update %s matching %j',
-	 //    ctx.Model.pluralModelName,
-	 //    ctx.where);
-
-	  if (ctx.instance) {
+		if (ctx.instance) {
 	  	//create
 	  	ctx.instance.created = new Date();
-	    ctx.instance.updated = new Date();
+	  	ctx.instance.updated = new Date();
 	  } else {
 	  	//update
 	  	ctx.data.updated = new Date();
