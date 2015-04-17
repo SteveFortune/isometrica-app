45659acb2978512fa873234a2017ace6e68c5be3
Mark Leusink <m.leusink@gmail.com>
2015-04-17 15:04:07 +0200

    Document library renamed to Document Wiki

--- client/datamodel/datamodel.js
+++ client/datamodel/datamodel.js
@@ -9,7 +9,7 @@ xcomponents.models['dataObject'] = {
   name : 'Data object',
   fields : [
     { label : 'Name' , field: 'name', required: true},
-    { label : 'Scope/module', field : 'scope', type : 'select', options : ['System', 'Overview module', 'Dashboard module', 'Document Library module', 'Workbook module']},
+    { label : 'Scope/module', field : 'scope', type : 'select', options : ['System', 'Overview module', 'Dashboard module', 'Document Wiki module', 'Workbook module']},
     { label : 'Attributes', field : 'attributes', type : 'multiline'},
     { label : 'Last update', field : 'updated', type : 'date', edit : false},
     { label : 'Details' , field: 'details', type : 'multiline' }
