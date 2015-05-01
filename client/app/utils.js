049bb3278d784eb214e1d97c9468c2de3ff260b2
Mark Leusink <m.leusink@gmail.com>
2015-05-01 11:32:09 +0200

    docWiki page editor now removes font/ span/ title tags, util function added to sanitize html (removing a specified list of disallowed tags)

--- client/app/utils.js
+++ client/app/utils.js
@@ -22,6 +22,39 @@ isa.utils = {
 				return 'fa fa-file-text-o';
 		}
 
+	},
+
+	htmlCleanup : function( htmlIn ) {
+		//cleans up (pasted) HTML code from (for instance) MS Word:
+		//removing all font/ changed size information
+
+		var NOT_ALLOWED = ["FONT", "TITLE", "SPAN"];
+
+		//setup a HTML element and inject the html
+		var div = document.createElement("div");
+    	div.innerHTML = htmlIn;
+
+    	//remove all disallowed tags
+    	var tags = Array.prototype.slice.apply(div.getElementsByTagName("*"), [0]);
+
+	    for (var i = 0; i < tags.length; i++) {
+	    	if ( NOT_ALLOWED.indexOf(tags[i].nodeName) > -1) {
+	    		this.removeNode(tags[i]);
+	    	}
+    	}
+
+    	return div.innerHTML;
+	},
+
+	removeNode : function(tag) {
+		//removes a node form a DOM object, attaching all childs to its parent
+		var last = tag;
+	    for (var i = tag.childNodes.length - 1; i >= 0; i--) {
+	        var e = tag.removeChild(tag.childNodes[i]);
+	        tag.parentNode.insertBefore(e, last);
+	        last = e;
+	    }
+	    tag.parentNode.removeChild(tag);
 	}
 
 };
