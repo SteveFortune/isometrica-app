f14948b0ab4f3acbbdf0b23842a07a0a92aa6adc
Mark Leusink <m.leusink@gmail.com>
2015-05-01 10:59:39 +0200

    moved the docWiki routes to a separate file

--- client/components/docWiki/docWikiController.js
+++ client/components/docWiki/docWikiController.js
@@ -12,53 +12,6 @@ var app = angular.module('isa.docwiki', [
 ]);
 
 /*
- * Define states for the Document Wiki module
- *
- * @author Mark Leusink
- */
-app.config(['$stateProvider', function($stateProvider){
-   
-	$stateProvider
-
-    	.state('docwiki', { 	
-		    url: '/docwiki/:planId',
-		    templateUrl: '/components/docWiki/docWikiView.html',
-		    controller : 'DocWikiController',
-		    data : {
-		    	anonymous: false
-		    }
-		})
-
-		.state('docwiki.newpage', { 	
-		    url: '/page/new',
-		    templateUrl: '/components/docWiki/page/pageEdit.html',
-		    controller : 'PageController',
-		    resolve : {
-		    	isNew : function() { return true; }
-		    }
-		})
-
-		.state('docwiki.page', { 	
-		    url: '/page/:pageId',
-		    templateUrl: '/components/docWiki/page/pageRead.html',
-		    controller : 'PageController',
-		    resolve : {
-		    	isNew : function() { return false; }
-		    }
-		})
-
-		.state('docwiki.pageedit', { 	
-		    url: '/page/:pageId/edit',
-		    templateUrl: '/components/docWiki/page/pageEdit.html',
-		    controller : 'PageController',
-		    resolve : {
-		    	isNew : function() { return false; }
-		    }
-		});
-
-}]);
-
-/*
  * Isometrica Document Wiki module
  *
  * @author Mark Leusink
---
+++ client/components/docWiki/docWikiRoutes.js
@@ -0,0 +1,48 @@
+var app = angular.module('isa.docwiki');
+
+/*
+ * Define states for the Document Wiki module
+ *
+ * @author Mark Leusink
+ */
+app.config(['$stateProvider', function($stateProvider){
+   
+	$stateProvider
+
+    	.state('docwiki', { 	
+		    url: '/docwiki/:planId',
+		    templateUrl: '/components/docWiki/docWikiView.html',
+		    controller : 'DocWikiController',
+		    data : {
+		    	anonymous: false
+		    }
+		})
+
+		.state('docwiki.newpage', { 	
+		    url: '/page/new',
+		    templateUrl: '/components/docWiki/page/pageEdit.html',
+		    controller : 'PageController',
+		    resolve : {
+		    	isNew : function() { return true; }
+		    }
+		})
+
+		.state('docwiki.page', { 	
+		    url: '/page/:pageId',
+		    templateUrl: '/components/docWiki/page/pageRead.html',
+		    controller : 'PageController',
+		    resolve : {
+		    	isNew : function() { return false; }
+		    }
+		})
+
+		.state('docwiki.pageedit', { 	
+		    url: '/page/:pageId/edit',
+		    templateUrl: '/components/docWiki/page/pageEdit.html',
+		    controller : 'PageController',
+		    resolve : {
+		    	isNew : function() { return false; }
+		    }
+		});
+
+}]);
