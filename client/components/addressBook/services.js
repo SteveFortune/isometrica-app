015f734e0049f80d86d59b0fc44fef92b2e11cd6
stephen fortune <steve.fortune@icecb.com>
2015-05-05 09:46:44 +0100

    1.1 Started building a UserService
    - Adopting Mark's remote/local architecture
    - Pondering a way to encapsulate the boilerplate of checking whether we're online or not
    - Perhaps some sort of abstract factory for all services ?

--- client/components/addressBook/services.js
+++ client/components/addressBook/services.js
@@ -1,2 +1,26 @@
 
 var app = angular.module('isa.addressbook.services');
+
+/**
+ * Basic service responsible for CRUD operations on users.
+ *
+ * @note As users might be quite commonly accessed throughout the application, it
+ * 		 might be worth having a base user service in the `app/` dir and inheriting
+ * 		 from it here.
+ * @note Can we encapsulate the boilerplate of checking whether we're online or not
+ * 		 in an abstract factory of sorts?
+ * @author Steve Fortune
+ */
+app.factory('UserService', ['$rootScope', '$injector', function($rootScope, $injector) {
+	return $rootScope.online ?
+		$injector.get('_UserServiceRemote') :
+		$injector.get('_UserServiceLocal');
+}]);
+
+app.factory('_UserServiceRemote', ['User', function(User) {
+
+}]);
+
+app.factory('_UserServiceLocal', [function() {
+
+}]);
