c8145deadbbae3099a797e747b8d819296d27271
stephen fortune <steve.fortune@icecb.com>
2015-05-11 11:52:37 +0100

    1.1 Started building in more fine-grained access control for users
    - Added a new acl to the IsometricaUser model
    - Started migrating the top-level authorization to its own service / run
    - This is so that it can be used by other services, factories, etc

--- client/app/services.js
+++ client/app/services.js
@@ -5,6 +5,8 @@ var app = angular.module('isa');
 
 /**
  * Utility service that encapsulates transforming a factory name
+ *
+ * @author Steve Fortune
  */
 app.service('PersistentFactoryNameResolver', ['$rootScope', function($rootScope) {
 	return {
@@ -13,4 +15,97 @@ app.service('PersistentFactoryNameResolver', ['$rootScope', function($rootScope)
 			return '_' + name + suffix;
 		}
 	};
+}]);
+
+
+/**
+ * Service that holds the current 'context' of the application. I.e. the
+ *
+ * @author Steve Fortune
+ */
+app.service('CurrentUser', ['IsometricaUser', function(IsometricaUser) {
+
+	/**
+	 * Current logged in user or null if not logged in
+	 *
+	 * @var Object | null
+	 */
+	var currentUser = null;
+
+	/**
+	 * Is the current user authenticated?
+	 *
+	 * @var Boolean
+	 */
+	var isAuthenticated = IsometricaUser.isAuthenticated();
+
+	/**
+	 * @param		user		Object
+	 * @private
+	 */
+	var setCurrentUser = function(user) {
+		currentUser = user;
+		isAuthenticated = !!currentUser;
+	};
+
+	return {
+
+		/**
+		 * @return Boolean
+		 */
+		isAuthenticated: function() {
+			return isAuthenticated;
+		},
+
+		/**
+		 * Attempts to retrieve the current user using the IsometricaUser service.
+		 *
+		 * @note How will this work on local devices?
+		 */
+		retrieveCurrentUser: function() {
+			IsometricaUser.getCurrent(function(user) {
+				setCurrentUser(user);
+			});
+		},
+
+		/**
+		 * @return	Object | null
+		 */
+		getCurrentUser: function() {
+			return currentUser;
+		}
+
+		/**
+		 * Is there a current user object? Alias for !!CurrentUser.getCurrentUser()
+		 *
+		 * @return Boolean
+		 */
+		hasCurrentUser: function() {
+			return !!currentUser;
+		}
+	};
+
+}]);
+
+
+/**
+ * Procedure that provides top-level, frontend authorization and that lazily loads
+ * the user using the CurrentUser service.
+ *
+ * @author Steve Fortune
+ */
+app.run(['$rootScope', 'CurrentUser', '$state', '$location', $function() {
+
+	$rootScope.$on('$stateChangeStart', function(event, toState, toParams) {
+		if (CurrentUser.hasUser()) {
+			return;
+		} else if (CurrentUser.isAuthenticated()) {
+			CurrentUser.retrieveCurrentUser();
+	    } else if (!toState.data.anonymous) {
+	        event.preventDefault();
+	        $location.nextAfterLogin = $location.path();
+	    	$state.go('login');
+		}
+	});
+
 }]);
