18f355a2aa92ffd0b2e4bac30ce6352684e9ceed
stephen fortune <steve.fortune@icecb.com>
2015-05-14 13:12:05 +0100

    1.1 Added validations to the IsometricaUser model

--- common/models/isometrica-user.js
+++ common/models/isometrica-user.js
@@ -22,4 +22,33 @@ module.exports = function(IsometricaUser) {
 
 	});
 
+	/**
+	 * Model validations
+	 *
+	 * @note Password validation: min 6 chars long containing both letters and numbers
+	 * @note Defining here rather than in the json file because the [docs say
+	 * 		 so](http://docs.strongloop.com/display/public/LB/Model+definition+JSON+file;jsessionid=D25089B559F634FAEA33260B9C331A20#ModeldefinitionJSONfile-Validations)
+	 * @author Steve Fortune
+	 */
+	IsometricaUser.validatesPresenceOf('firstName', 'lastName', 'email', 'password');
+	IsometricaUser.validatesUniquenessOf('email', {
+		message: 'A user is already registered with this email address.'
+	});
+	IsometricaUser.validatesFormatOf('email', {
+		// @note This regex is copied straight from the Loopback documentation for validating
+		// an email address here: http://docs.strongloop.com/display/public/LB/Validating+model+data.
+		// In the future it might be worth registering a custom validator to do a more sophisticated
+		// check.
+		with: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+		message: 'Email address should be in the format local-part@host' 
+	})
+	IsometricaUser.validatesLengthOf('password', {
+		min: 6,
+		message: 'Password must be > 6 characters long'
+	});
+	IsometricaUser.validatesFormatOf('password', {
+		with: /^(?=.*[a-zA-Z])(?=.*[0-9])/,
+		message: 'Password must be > 6 characters long'
+	});
+
 };
