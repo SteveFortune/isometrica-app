3e72a3aea2720944eab2a7a1d6b8f06752e63416
Mark Leusink <m.leusink@gmail.com>
2015-05-15 17:29:04 +0200

    docwiki: Signature model & remote 'sign' method for a page created

--- common/models/page.js
+++ common/models/page.js
@@ -1,3 +1,45 @@
 module.exports = function(Page) {
 
+	/*
+	 * Remote method to sign a page: adds the current user & timestamp to a list of signers on the page
+	 * related pages.
+	 *
+	 * @author Mark Leusink
+	 */
+	Page.sign = function(pageId, cb) {
+
+		var loopback = require('loopback');
+		var Signature = loopback.findModel('Signature');
+		
+		Page.findById( pageId, function(err, page) {
+
+			if (err) {
+				console.error(err);
+			}
+
+			//TODO: get current user
+			Signature.create({ created : new Date(), createdBy : 'Mark Leusink'}, function(err, signature) {
+				console.log('add', signature);
+				page.signatures.add(signature);
+				
+			});
+
+			cb(null);
+
+		});
+
+	};
+
+	//register the remote method
+	Page.remoteMethod(
+		'sign',
+		{
+			'accepts': [
+   				{arg: 'pageId', type: 'string', required: true}
+   			],
+   			'returns': {arg: 'message', type: 'string'},
+			'description' : 'Sign a page'
+		}
+	);
+
 };
